bda4112de405430103ef7bc035575bd3166abe64
Omar Santos Yujra Huanca <OmYujra@bnb.com.bo>
2023-09-26 07:31:56 -0400

    En este tutorial vamos a crear una Web Api en .NET 6 utilizando APIs mínimas (minimal APIs) y además escribiremos en una base de datos PostgreSQL utilizando Entity Framework Code First.

---
+++ MinimalAPIPostgresSQLEF/Data/OfficeDb.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MinimalAPIPostgresSQLEF.Model;
+
+namespace MinimalAPIPostgresSQLEF.Data
+{
+    public class OfficeDb: DbContext
+    {
+        public OfficeDb(DbContextOptions<OfficeDb> options): base(options) 
+        {
+
+        }
+        public DbSet<Employee> Employees => Set<Employee>();
+    }
+}
---
+++ MinimalAPIPostgresSQLEF/Model/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace MinimalAPIPostgresSQLEF.Model
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Branch { get; set; }
+        public int Age { get; set; }
+
+    }
+}
